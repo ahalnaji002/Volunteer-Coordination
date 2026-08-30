4846bbde693e93c456e64f2d7c6289ab6cf080cc
Ahmed Hashem <ahalnaji002@gmail.com>
2026-08-31 00:28:37 +0300

    add render seeder toggle

--- docker/render-start.sh
+++ docker/render-start.sh
@@ -13,4 +13,8 @@ if [ "${RUN_MIGRATIONS:-false}" = "true" ]; then
     php artisan migrate --force
 fi
 
+if [ "${RUN_SEEDERS:-false}" = "true" ]; then
+    php artisan db:seed --force
+fi
+
 exec apache2-foreground
